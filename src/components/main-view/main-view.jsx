6eb61c70c6f0615cd59c02abd7f12926dca4e72f
Turo-Rudi <jozsa.andrea@gmail.com>
2021-07-20 20:08:40 +0200

    Revert "implement nav-view"
    This reverts commit 5f9ed08d27b6ef399b19b9957520c382a9eedb9c.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 
@@ -7,16 +8,18 @@ import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import { setMovies, setUser } from '../../actions/actions';
 import MoviesList from '../movies-list/movies-list';
+
+//import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { ProfileView } from '../profile-view/profile-view';
 import { DirectorView } from '../director-view/director-view';
 import { GenreView } from '../genre-view/genre-view';
-import { NavView } from '../nav-view/nav-view';
+import { NavBar } from '../nav-bar/nav-bar';
 
 import './main-view.scss';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button, Navbar } from 'react-bootstrap';
 
 class MainView extends React.Component {
 
@@ -93,13 +96,14 @@ class MainView extends React.Component {
     let { movies, user } = this.props;
 
     return (
+
       <Router>
-        <Row>
-          <Col>
-            <NavView />
-          </Col>
-        </Row>
+        <Col>
+          <NavBar />
+        </Col>
+
         <Row className="main-view justify-content-md-center">
+
           <Route exact path="/" render={() => {
             if (!Username) return <Col>
               <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
@@ -115,7 +119,7 @@ class MainView extends React.Component {
             </Col>
           }} />
 
-          <Route path="/users/:userId" render={({ history }) => {
+          <Route path="/users/:userId" render={({ match, history }) => {
             if (movies.length === 0) return <div className="main-view">Loading!</div>;
             if (Username) return <Col>
               <ProfileView onLoggedIn={user => this.onLoggedIn(user)}
@@ -124,8 +128,9 @@ class MainView extends React.Component {
           }} />
 
           <Route path="/movies/:movieId" render={({ match, history }) => {
-            if (!Username) return
-            <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            if (!Username) return <Col>
+              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
             if (movies.length === 0) return <div className="main-view">Loading!</div>;
             return <Col md={8}>
               <MovieView FavoriteMovies={user.FavoriteMovies} movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
@@ -143,8 +148,9 @@ class MainView extends React.Component {
           }} />
 
           <Route path="/genres/:name" render={({ match, history }) => {
-            if (!Username) return
-            <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            if (!Username) return <Col>
+              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
             if (movies.length === 0) return <div className="main-view">Loading!</div>;
             return <Col md={8}>
               <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
