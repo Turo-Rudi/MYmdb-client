f97bea4f9082a8d36367daba19ad1b93d6231570
Turo-Rudi <jozsa.andrea@gmail.com>
2021-07-20 20:06:58 +0200

    Revert "undo "routes in separate file""
    This reverts commit 75b6e65c931615d0b698cf4997820fc45ce833d1.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,14 +8,13 @@ import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import { setMovies, setUser } from '../../actions/actions';
 import MoviesList from '../movies-list/movies-list';
-
-//import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { ProfileView } from '../profile-view/profile-view';
 import { DirectorView } from '../director-view/director-view';
 import { GenreView } from '../genre-view/genre-view';
+import { NavView } from '../nav-view/nav-view';
 
 import './main-view.scss';
 import { Row, Col, Button, Navbar } from 'react-bootstrap';
@@ -125,61 +124,12 @@ class MainView extends React.Component {
           </Navbar.Collapse>
         </Navbar>
 
-        <Row className="main-view justify-content-md-center">
-          <Route exact path="/" render={() => {
-            if (!Username) return <Col>
-              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
-            </Col>
-            if (movies.length === 0) return <div className="main-view">Loading!</div>;
-            return <MoviesList movies={movies} />
-          }} />
-
-          <Route path="/register" render={() => {
-            if (Username) return <Redirect to="/" />
-            return <Col>
-              <RegistrationView />
-            </Col>
-          }} />
-
-          <Route path="/users/:userId" render={({ match, history }) => {
-            if (movies.length === 0) return <div className="main-view">Loading!</div>;
-            if (Username) return <Col>
-              <ProfileView onLoggedIn={user => this.onLoggedIn(user)}
-                movies={movies} user={user} onBackClick={() => history.goBack()} />
-            </Col>
-          }} />
-
-          <Route path="/movies/:movieId" render={({ match, history }) => {
-            if (!Username) return <Col>
-              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
-            </Col>
-            if (movies.length === 0) return <div className="main-view">Loading!</div>;
-            return <Col md={8}>
-              <MovieView FavoriteMovies={user.FavoriteMovies} movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
-            </Col>
-          }} />
-
-          <Route path="/directors/:name" render={({ match, history }) => {
-            if (!Username) return <Col>
-              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
-            </Col>
-            if (movies.length === 0) return <div className="main-view">Loading!</div>;
-            return <Col md={8}>
-              <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} onBackClick={() => history.goBack()} />
-            </Col>
-          }} />
-
-          <Route path="/genres/:name" render={({ match, history }) => {
-            if (!Username) return <Col>
-              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
-            </Col>
-            if (movies.length === 0) return <div className="main-view">Loading!</div>;
-            return <Col md={8}>
-              <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
-            </Col>
-          }} />
-
+        <Row>
+          <Col>
+            <NavView />
+          </Col>
         </Row>
+
       </Router>
     );
   }
